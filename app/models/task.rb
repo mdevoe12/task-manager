adf9ec4b0994ba6ba4cf2ab8304e282bd54bde24
Matt DeVoe <matthew.devoe@gmail.com>
2017-06-23 12:25:39 -0600

    Set up read/write to database

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,11 @@ class Task
   def intitialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
-    @database    = SQLite3::Database.new('db/task_manager_development.db')
+    @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
+  end
+
+  def save
+    @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
   end
 end
