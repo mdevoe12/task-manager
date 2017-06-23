f394cf49b62879c8343975eecdab061d974d9419
Matt DeVoe <matthew.devoe@gmail.com>
2017-06-23 12:40:30 -0600

    Updated task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 require 'sqlite3'
 
 class Task
-  def intitialize(task_params)
+  def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
@@ -10,5 +10,14 @@ class Task
 
   def save
     @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
+  end
+
+  def self.all
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    tasks = database.execute("SELECT * FROM tasks")
+    tasks.map do |task|
+      Task.new(task)
+    end
   end
 end
